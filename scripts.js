c21f567d33ee0fdd35a7e855ff5eda837212b561
Alondradaisy <alondra.warren96@gmail.com>
2021-10-24 20:50:58 -0700

    button click functionality for adding and removing nums

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
-const counter = 0;
+let counter = 0;
+
+function logCounter() {
+  console.log("counter: ", counter);
+}
 
 function add() {
+  //console.log("running");
   counter = counter + 1;
+  numberDisplay;
+  logCounter();
 }
 
 function remove() {
+  //console.log("running");
   counter = counter - 1;
+  numberDisplay();
+  logCounter();
 }
 
-function deleteDaisy() {}
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", add);
+
+const removeButton = document.querySelector("#remove");
+removeButton.addEventListener("click", remove);
+
+const numberCounter = document.querySelector("#number-counter");
+
+function numberDisplay() {
+  numberCounter.innerText = counter;
+}
